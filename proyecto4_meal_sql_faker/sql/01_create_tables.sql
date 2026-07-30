4e4822e1b3eb203af4294634cdf2adba7969d99d
portafolio-andrea-rojas <andrearojasmendez23@gmail.com>
2026-07-30 12:02:07 +0200

    Update 01_create_tables.sql
    sql: crear base de datos y tabla proyectos

--- proyecto4_meal_sql_faker/sql/01_create_tables.sql
+++ proyecto4_meal_sql_faker/sql/01_create_tables.sql
@@ -1,1 +1,24 @@
+-- Proyecto: Territorios que Dialogan
+-- Base de datos MEAL para un programa de construcción de paz
+-- Motor compatible con MySQL: TiDB
 
+CREATE DATABASE IF NOT EXISTS territorios_que_dialogan;
+
+USE territorios_que_dialogan;
+
+-- Tabla: proyectos
+-- Granularidad: una fila representa un proyecto
+
+CREATE DATABASE IF NOT EXISTS territorios_que_dialogan;
+USE territorios_que_dialogan;
+SELECT DATABASE();
+CREATE TABLE proyectos (
+  id_proyecto INT AUTO_INCREMENT PRIMARY KEY,
+  codigo_proyecto VARCHAR(10) NOT NULL UNIQUE KEY, 
+  nombre_proyecto VARCHAR(150) NOT NULL,
+  objetivo_especifico TEXT NOT NULL,
+  fecha_inicio DATE NOT NULL,
+  fecha_fin DATE NOT NULL,
+  presupuesto_aprobado DECIMAL(10,2) NOT NULL,
+  estado VARCHAR(30) NOT NULL
+);
