da60eec61c9562f2441b478a22cb6a53b394353c
portafolio-andrea-rojas <andrearojasmendez23@gmail.com>
2026-08-03 11:05:51 +0200

    Create 03_analysis_queries.sql
    sql: agregar primera consulta de análisis con joins

---
+++ proyecto4_meal_sql_faker/sql/03_analysis_queries.sql
@@ -0,0 +1,29 @@
+-- =====================================================
+-- Proyecto: Territorios que Dialogan
+-- Archivo: 03_analysis_queries.sql
+-- Descripción: consultas SQL para análisis MEAL
+-- =====================================================
+
+USE territorios_que_dialogan;
+
+-- =====================================================
+-- Consulta 1
+-- Pregunta:
+-- ¿Qué actividades se realizaron, a qué proyecto
+-- pertenecen y en qué territorio se desarrollaron?
+-- =====================================================
+
+SELECT 
+a.id_actividad,
+a.codigo_actividad,
+a.nombre_actividad,
+p.codigo_proyecto,
+p.nombre_proyecto,
+t.departamento,
+t.municipio
+FROM actividades AS a
+INNER JOIN proyectos AS p
+ON a.id_proyecto = p.id_proyecto
+INNER JOIN territorios AS t
+ON a.id_territorio = t.id_territorio
+ORDER BY a.codigo_actividad;
